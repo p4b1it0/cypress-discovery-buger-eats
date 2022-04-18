fa9d7be274b1a4e654585803631cbafc22764207
p4b1it0 <cabreir@outlook.com>
2022-04-17 23:05:57 -0300

    Inserindo parâmetros em cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,9 @@
+// Aula de criação da primeira suíte de testes
+
 describe('home page', () => {
     it('app deve estar online', () => {
-        cy.viewport(1280, 720)
-        cy.visit('https://buger-eats.vercel.app')
+        // cy.viewport(1280, 720) // Isto pode ser inserido no "cypress.json"
+        cy.visit('/') // O endereço foi referenciado em "cypress.json"
         cy.get('#page-home div h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     })
 })
